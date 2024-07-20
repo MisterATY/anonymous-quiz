58247c99b705d2b2783a976bcb8ef3d5027f368d
Mister ATY <misteraty25@gmail.com>
2024-07-20 23:43:44 +0500

    vercel11

--- pages/api/check.js
+++ pages/api/check.js
@@ -1,13 +1,18 @@
 import { promises as fs } from 'fs';
 import path from 'path';
 
+const getClientIp = (req) => {
+  return req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+};
+
 const submissionsFile = path.join('/tmp', 'submissions.json');
 
 export default async function handler(req, res) {
   try {
     const data = await fs.readFile(submissionsFile, 'utf8');
     const submissions = JSON.parse(data);
-    res.status(200).json({ submissions });
+    const ip = getClientIp(req);
+    res.status(200).json({ submissions, ip });
   } catch (err) {
     if (err.code === 'ENOENT') {
       return res.status(200).json({ submissions: [] });
