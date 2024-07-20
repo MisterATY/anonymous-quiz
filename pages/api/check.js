52abe1f08b16d25d19b5724eb9c4777d2ccf8bd3
Mister ATY <misteraty25@gmail.com>
2024-07-20 23:40:09 +0500

    vercel10

--- pages/api/check.js
+++ pages/api/check.js
@@ -1,24 +1,17 @@
-// pages/api/check.js
 import { promises as fs } from 'fs';
 import path from 'path';
 
-const getClientIp = (req) => {
-  return req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-};
+const submissionsFile = path.join('/tmp', 'submissions.json');
 
-export default function handler(req, res) {
-  const ip = getClientIp(req);
-  // const filePath = path.join(process.cwd(), 'data', 'submissions.json');
-  const submissionsFile = path.join('/tmp', 'submissions.json');
-  // const submissions = JSON.parse(fs.readFileSync(filePath, 'utf8'));
-  const data = fs.readFile(submissionsFile, 'utf8');
-  const submissions = JSON.parse(data);
-
-  const alreadySubmitted = submissions.some(submission => submission.ip === ip);
-
-  if (alreadySubmitted) {
-    res.status(200).json({ message: 'Already submitted', fufu: submissions });
-  } else {
-    res.status(404).json({ message: 'Not found', fufu: submissions });
+export default async function handler(req, res) {
+  try {
+    const data = await fs.readFile(submissionsFile, 'utf8');
+    const submissions = JSON.parse(data);
+    res.status(200).json({ submissions });
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      return res.status(200).json({ submissions: [] });
+    }
+    res.status(500).json({ error: 'Error reading submissions file' });
   }
 }
